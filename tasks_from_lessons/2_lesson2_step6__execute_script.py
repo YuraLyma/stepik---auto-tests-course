d476dda14a3f953b7600b0762232aac72205e577
yura.lyma@gmail.com <yura.lyma@gmail.com>
2020-12-23 18:12:20 +0200

    2_lesson2_step6__execute_script

---
+++ tasks_from_lessons/2_lesson2_step6__execute_script.py
@@ -0,0 +1,31 @@
+import math
+import time
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+	driver = webdriver.Chrome()
+	driver.get('http://suninjuly.github.io/execute_script.html')
+
+	x = driver.find_element_by_id('input_value').text.strip()
+	x = calc(x)
+
+	driver.execute_script("window.scrollBy(0, 200);")
+
+	driver.find_element_by_id("answer").send_keys(x)
+	driver.find_element_by_id("robotCheckbox").click()
+	driver.find_element_by_id("robotsRule").click()
+
+	driver.find_element_by_css_selector("[type='submit']").click()
+
+	
+finally:
+	time.sleep(5)
+	driver.quit()
